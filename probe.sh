1f80fbe508c6e4ba8e433804258ef63343be8445
Willem Essenstam <wessenstam@gmail.com>
2020-03-26 17:51:01 +0100

    Update the location of the json file

--- probe.sh
+++ probe.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # Script to pull the latest of the web_server into the container on start
+rm -Rf /github
 mkdir -p /github
 cd /github
 git clone https://github.com/wessenstam/web_gts
